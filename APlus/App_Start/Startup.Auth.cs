951b3276b5f44d8d5366c57ef2e70db3b7fa005b
md.irfan00@gmail.com <F@rzi123>
2016-04-22 17:47:27 -0700

    Initial Add into Git

---
+++ APlus/App_Start/Startup.Auth.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace APlus
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            //published
+            //app.UseFacebookAuthentication(
+            //   appId: "2953698163",
+            //   appSecret: "c7429c59bada5c5a359649d746340f19");
+            //localhost
+            app.UseFacebookAuthentication(
+               appId: "2694467493",
+               appSecret: "81901e381f383687ff44d508ee792a70");
+
+            app.UseGoogleAuthentication();
+        }
+    }
+}
---
+++ APlus/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APlus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "APlusPoint - Platform to Excel ...";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "APlusPoint - Contact Us";
+
+            return View();
+        }
+    }
+}
